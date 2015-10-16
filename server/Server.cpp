ec51437857821587ecf1690f43cae462dbe07c43
XMs <xms@nm.ru>
2015-10-16 15:09:53 +0300

    server: server

---
+++ server/Server.cpp
@@ -0,0 +1,302 @@
+#include <iostream>
+
+#include "LEDDevice.h"
+#include "Server.h"
+
+
+
+Server::Server(): m_pDevice(nullptr),
+                  m_prevColor(Color::Off)
+  {}
+
+
+
+Server::~Server()
+  {
+	delete m_pDevice;
+  }
+
+
+
+/***********
+* Private *
+ ***********/
+
+
+
+/*************
+ * Protected *
+ *************/
+void Server::log(const std::string &msg)
+  {
+	std::cerr << msg;
+  }
+
+
+
+/**********
+ * Public *
+ **********/
+/*** Set ***/
+void Server::setDevice(LEDDevice *device)
+  {
+	m_pDevice = device;
+  }
+
+
+
+bool Server::setLEDState(const std::string &state)
+  {
+	if (state.compare("on") == 0)
+		return setLEDState(true);
+
+	if (state.compare("off") == 0)
+		return setLEDState(false);
+
+	log("setLEDState: Unknown argument " + state);
+	return false;
+  }
+
+
+
+bool Server::setLEDColor(const std::string &color)
+  {
+	if (color.compare("red")   == 0)
+		return setLEDColor(Color::Red);
+
+	if (color.compare("green") == 0)
+		return setLEDColor(Color::Green);
+
+	if (color.compare("blue")  == 0)
+		return setLEDColor(Color::Blue);
+
+	log("setLEDState: Unknown argument " + color);
+	return false;
+  }
+
+
+
+bool Server::setLEDRate(const std::string &rate)
+  {
+	if (m_pDevice == nullptr) return false;
+
+	uchar nrate;
+
+	try
+	  {
+		nrate = uchar(std::stoi(rate));
+	  }
+	catch (std::exception exp)
+	  {
+		log("setLEDRate: " + std::string(exp.what()));
+		return false;
+	  }
+
+	return setLEDRate(nrate);
+  }
+
+
+
+bool Server::setLEDState(bool state)
+  {
+	if (m_pDevice == nullptr) return false;
+
+	if (state)
+	  {
+		if (!getLEDState())
+			return m_pDevice->setColor(m_prevColor);
+	  }
+	else
+	  {
+		if (getLEDState())
+		  {
+			m_prevColor = getLEDColor();
+			return m_pDevice->setColor(Color::Off);
+		  }
+	  }
+
+	return false;
+  }
+
+
+
+bool Server::setLEDColor(Server::Color color)
+  {
+	return m_pDevice->setColor(color);
+  }
+
+
+
+bool Server::setLEDRate(uchar rate)
+  {
+	return m_pDevice->setFrequency(rate);
+  }
+
+
+
+/*** Get ***/
+LEDDevice *Server::device() const
+  {
+	return m_pDevice;
+  }
+
+
+
+std::string Server::getLEDStateStr() const
+  {
+	if (m_pDevice == nullptr) goto fail;
+	else
+	  {
+		bool failed;
+		bool result = getLEDState(&failed);
+
+		if (failed) goto fail;
+
+		if (result) return std::string("OK on");
+		else return std::string("OK off");
+	  }
+
+  fail:
+	return std::string("FAILED");
+  }
+
+
+
+std::string Server::getLEDColorStr() const
+  {
+	if (m_pDevice == nullptr) goto fail;
+	else
+	  {
+		bool failed;
+		Color color = getLEDColor(&failed);
+
+		if (failed) goto fail;
+
+		switch(color)
+		  {
+			case Color::Blue:
+				return std::string("OK blue");
+
+			case Color::Green:
+				return std::string("OK green");
+
+			case Color::Red:
+				return std::string("OK red");
+
+			default: goto fail;
+		  }
+	  }
+
+  fail:
+	return std::string("FAILED");
+  }
+
+
+
+std::string Server::getLEDRateStr() const
+  {
+	if (m_pDevice == nullptr) goto fail;
+	else
+	  {
+		bool failed;
+		uchar rate = getLEDRate(&failed);
+
+		if (failed) goto fail;
+
+		return std::string("OK ").append(std::to_string(rate));
+	  }
+
+  fail:
+  	return std::string("FAILED");
+  }
+
+
+
+bool Server::getLEDState(bool *fail) const
+  {
+	if (m_pDevice == nullptr) goto fail;
+
+	return m_pDevice->color() != Color::Off;
+
+  fail:
+	if (fail) *fail = true;
+	return false;
+  }
+
+
+
+Server::Color Server::getLEDColor(bool *fail) const
+  {
+	if (m_pDevice == nullptr) goto fail;
+
+	return m_pDevice->color();
+
+  fail:
+	if (fail) *fail = true;
+	return Color::Off;
+  }
+
+
+
+uchar Server::getLEDRate(bool *fail) const
+  {
+	if (m_pDevice == nullptr) goto fail;
+
+	return m_pDevice->frequency();
+
+  fail:
+	if (fail) *fail = true;
+	return 0;
+  }
+
+
+
+/*** Other ***/
+bool Server::applyCommand(const std::string &cmd,
+                          const std::string &arg,
+                                std::string *res)
+  {
+	auto it = m_ops.find(cmd);
+	if (it == m_ops.end()) goto unknown_command;
+
+	switch(it->second)
+	  {
+		case GetLEDState:
+			*res = getLEDStateStr();
+			return true;
+
+		case GetLEDColor:
+			*res = getLEDColorStr();
+			return true;
+
+		case GetLEDRate:
+			*res = getLEDRateStr();
+			return true;
+
+		case SetLEDState:
+			if (setLEDState(arg)) goto set_ok;
+			else                  goto set_fail;
+
+		case SetLEDColor:
+			if (setLEDColor(arg)) goto set_ok;
+			else                  goto set_fail;
+
+		case SetLEDRate:
+			if (setLEDRate(arg))  goto set_ok;
+			else                  goto set_fail;
+	  }
+
+  unknown_command:
+	log("Unknown command" + cmd);
+
+	if (res) *res = "FAILED";
+	return false;
+
+  set_ok:
+	*res = "OK";
+	return true;
+
+  set_fail:
+	*res = "FAILED";
+	return true;
+  }
---
+++ server/Server.h
@@ -0,0 +1,76 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <map>
+
+#include "LEDDevice.h"
+
+
+
+class Server
+  {
+	private:
+		enum Operation
+		  {
+			SetLEDState,
+			GetLEDState,
+
+			SetLEDColor,
+			GetLEDColor,
+
+			SetLEDRate,
+			GetLEDRate,
+		  };
+
+		const std::map<std::string, Operation> m_ops =
+			{{"set-led-state", SetLEDState},
+			 {"get-led-state", GetLEDState},
+
+			 {"set-led-color", SetLEDColor},
+			 {"get-led-color", GetLEDColor},
+
+			 {"set-led-rate",  SetLEDRate},
+			 {"get-led-rate",  GetLEDRate},
+			};
+
+		typedef LEDDevice::Color Color;
+
+		LEDDevice *m_pDevice;
+		Color      m_prevColor;
+
+	protected:
+		void log(const std::string &msg);
+
+	public:
+		explicit Server();
+		virtual ~Server();
+
+		/*** Set ***/
+		void         setDevice(LEDDevice *device);
+
+		bool         setLEDState(const std::string &state);
+		bool         setLEDColor(const std::string &color);
+		bool         setLEDRate(const std::string &rate);
+
+		bool         setLEDState(bool state);
+		bool         setLEDColor(Color color);
+		bool         setLEDRate(uchar rate);
+
+		/*** Get ***/
+		LEDDevice   *device()         const;
+
+		std::string  getLEDStateStr() const;
+		std::string  getLEDColorStr() const;
+		std::string  getLEDRateStr()  const;
+
+		bool         getLEDState(bool *fail = nullptr)    const;
+		Color        getLEDColor(bool *fail = nullptr)    const;
+		uchar        getLEDRate(bool  *fail = nullptr)    const;
+
+		/*** Other ***/
+		bool         applyCommand(const std::string &cmd,
+		                          const std::string &arg,
+		                                std::string *res);
+  };
+
+#endif // SERVER_H
